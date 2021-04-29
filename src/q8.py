caa13a213f5553c817d26d017709a19cc74722a3
shruti <kshruti14101998@gmail.com>
2021-04-29 15:20:42 +0530

    Added

--- src/q8.py
+++ src/q8.py
@@ -1,7 +1,8 @@
   
 """ value is a empty list """
-VALUE = []
-for number in range(1, 21):
-    VALUE.append(number)
-SQUARED_NUMBER = map(lambda x: x**2, VALUE)
-print(list(SQUARED_NUMBER))
+class squareMap:
+    VALUE = []
+    for number in range(1, 21):
+        VALUE.append(number)
+    SQUARED_NUMBER = map(lambda x: x**2, VALUE)
+    print(list(SQUARED_NUMBER))
--- test/tests.py
+++ test/tests.py
@@ -1,18 +1,31 @@
 import unittest
-from unittest.mock import patch
 from io import StringIO
-from src.q8 import q8
+from unittest.mock import patch
+
+from src.q10 import Reverselter
+
 
 class TestFunctions(unittest.TestCase):
 
-    def swtUp(self):
+    def setUp(self):
         pass
 
     def test_q8(self):
         expected = [i*i for i in range (1,21)]
         actual=squareMap()
-        self.asserteual(expected,actual)
+        self.assertEqual(expected,actual)
+    
+    def test_iterClass(self):
+        expected =[]
+        for ele in ReverseIter([i*i for i in range (1,21)]):
+            expected.append(ele)
+        actual = reversed([i*i for i in range(1,21)])
+        self.assertEqual(liist(expected),list(acual))
     
     def tearDown(self):
         print("Testing completed")
+
+
+if __name__ == '__main__':
+    unittest.main()
 
